bff0d7829eec515f920fa15bb805c7b69aaa8912
DaBiggestPhrog <32445295-DaBiggestPhrog@users.noreply.replit.com>
2024-04-17 14:38:58 +0000

    Zad 3_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Repozytorium nr 3");
+    Scanner scanner = new Scanner(System.in);
+
+    // zapytanie o znak
+    System.out.print("Podaj znak: ");
+    char znak = scanner.next().charAt(0);
+
+    // zapytanie o rozmiar
+    System.out.print("Podaj rozmiar: ");
+    int rozmiar = scanner.nextInt();
+
+    // rysowanie
+    for (int i = 0; i < rozmiar; i++) {
+      for (int j = 0; j < rozmiar; j++) {
+        System.out.print(znak + " ");
+      }
+      System.out.println();
+    }
   }
 }
